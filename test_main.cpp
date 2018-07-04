d6519b74c90848d9b00c4a5a00a4a3551d11599e
AZ015 <elektronchikone@mail.ru>
2018-07-04 07:14:55 +0700

    Last 6

--- test_main.cpp
+++ test_main.cpp
@@ -1,12 +1,13 @@
-#include "lib.h"
 
 #define BOOST_TEST_MODULE test_main
 
+#include "lib.h"
+
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE (test_suite_main)
+BOOST_AUTO_TEST_SUITE (test_version)
 
-BOOST_AUTO_TEST_CASE(test_version_valid)
+BOOST_AUTO_TEST_CASE(test_valid_version)
 
 {
 	BOOST_CHECK(version() > 0);
